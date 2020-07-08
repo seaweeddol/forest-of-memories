e0f08eb73fb7050e848aa1287995ccdd551c907c
Jessica Stone <jstone4815@gmail.com>
2020-07-07 21:32:03 -0700

    add simple show/hide function for add memory ui

---
+++ Assets/Scripts/DisplayMemoryUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DisplayMemoryUI : MonoBehaviour
+{
+    public GameObject memoryUI;
+
+    bool m_MemoryUIVisible = false;
+
+    // Update is called every frame
+    void Update () {
+        if(Input.GetKeyDown("space")) {
+            if (!m_MemoryUIVisible) {
+                m_MemoryUIVisible = true;
+                memoryUI.SetActive(true);
+                return;
+            }
+        }
+
+        if(Input.GetKeyDown("escape")) {
+            if (m_MemoryUIVisible) {
+                m_MemoryUIVisible = false;
+                memoryUI.SetActive(false);
+                return;
+            }
+        }
+    }
+
+}
