f8ed6246149d5fddbb6bc1ff79e350995a809259
Jessica Stone <jstone4815@gmail.com>
2020-07-09 16:54:35 -0700

    change cursor when user hovers over a tree

---
+++ Assets/Scripts/TreeInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeInteraction : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void OnMouseOver(){
+        Debug.Log("it's angry");
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode); 
+    }
+
+    void OnMouseExit(){
+        Debug.Log("it's not angry");
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+}
