5e2f1cdd92fb7b4145402d691a1813e215ab6dc1
Jessica Stone <jstone4815@gmail.com>
2020-07-09 13:57:17 -0700

    prevent player from moving if memory UI is visible

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
 
+    public Canvas m_MemoryUI;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+
 
     void Start ()
     {
@@ -20,30 +22,31 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate ()
     {
         // TODO: if memory UI is visible, player should not be able to walk
+        if(!m_MemoryUI.isActiveAndEnabled) {
+            float horizontal = Input.GetAxis ("Horizontal");
+            float vertical = Input.GetAxis ("Vertical");
+            
+            m_Movement.Set(horizontal, 0f, vertical);
+            m_Movement.Normalize ();
 
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize ();
+            bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
+            bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
+            bool isWalking = hasHorizontalInput || hasVerticalInput;
+            m_Animator.SetBool ("isWalking", isWalking);
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool ("isWalking", isWalking);
-
-        // play footstep sounds if player is walking
-        bool footsteps = GetComponent<AudioSource>().isPlaying;
-        if (isWalking) {
-            if (!footsteps) {
-                GetComponent<AudioSource>().Play();
+            // play footstep sounds if player is walking
+            bool footsteps = GetComponent<AudioSource>().isPlaying;
+            if (isWalking) {
+                if (!footsteps) {
+                    GetComponent<AudioSource>().Play();
+                }
+            } else {
+                GetComponent<AudioSource>().Pause();
             }
-        } else {
-            GetComponent<AudioSource>().Pause();
-        }
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
+            Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation (desiredForward);
+        }
     }
 
     void OnAnimatorMove ()
