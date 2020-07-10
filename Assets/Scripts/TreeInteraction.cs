bc9f85e27e65152fc8e0cf4621e4292ac9ca14fb
Jessica Stone <jstone4815@gmail.com>
2020-07-09 20:49:23 -0700

    make memory text appear on tree click

--- Assets/Scripts/TreeInteraction.cs
+++ Assets/Scripts/TreeInteraction.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TreeInteraction : MonoBehaviour
 {
     public Texture2D cursorTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
+    public GameObject memoryTextContainer;
+    public GameObject memoryText;
 
     public double score;
     public string tone;
@@ -21,6 +24,10 @@ public class TreeInteraction : MonoBehaviour
     }
 
     void OnMouseDown(){
-        Debug.Log(memory);
+        // TODO: show text in front of tree
+        // hide text on click or after a certain distance
+        TextMeshProUGUI textMeshPro = memoryText.GetComponent<TextMeshProUGUI>();
+        textMeshPro.SetText(memory);
+        memoryTextContainer.SetActive(true);
     }
 }
