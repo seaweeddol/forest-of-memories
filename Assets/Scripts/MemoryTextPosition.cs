4c3edc87d235b0cb3299a68dc292390ad2c732bf
Jessica Stone <jstone4815@gmail.com>
2020-07-11 16:50:16 -0700

    TEXT IS FADING IN AND OUT ONE TIME

--- Assets/Scripts/MemoryTextPosition.cs
+++ Assets/Scripts/MemoryTextPosition.cs
@@ -7,11 +7,15 @@ using TMPro;
 public class MemoryTextPosition : MonoBehaviour
 {
     public Transform player;
-    public GameObject memoryContainer;
+    // public GameObject memoryContainer;
     public GameObject memory;
     public Collider collider;
     public TreeInfo treeInfo;
+    bool m_IsTextVisible = false;
     bool m_IsPlayerInRange;
+    // public float alpha;
+    float waitTime = 2.0f;
+    float timer = 0.0f;
 
     void Start() {
         memory.GetComponent<TextMeshProUGUI>().text = treeInfo.memory;
@@ -19,14 +23,23 @@ public class MemoryTextPosition : MonoBehaviour
 
     void Update (){
         // TODO: make text move with camera aim
-
-        // show memory if user is within range of tree
-        if(m_IsPlayerInRange) {
-            // canvas.LookAt(player.transform.position);
-            memoryContainer.SetActive(true);
-        } else {
-            memoryContainer.SetActive(false);
+        if (m_IsPlayerInRange && !m_IsTextVisible) {
+            timer += Time.deltaTime;
+            if (timer < waitTime) {
+                memory.GetComponent<TextMeshProUGUI>().alpha = timer / 2;
+                Debug.Log("current alpha: " + memory.GetComponent<TextMeshProUGUI>().alpha);
+            } else {
+                m_IsTextVisible = true;
+                timer = 0.0f;
+            }
+        } else if (!m_IsPlayerInRange && m_IsTextVisible) {
+            timer += Time.deltaTime;
+            if (timer < waitTime) {
+                memory.GetComponent<TextMeshProUGUI>().alpha -= timer / 2;
+                Debug.Log("current alpha: " + memory.GetComponent<TextMeshProUGUI>().alpha);
+            }
         }
+        // show memory if user is within range of tree
     }
 
     void OnTriggerEnter (Collider other) {
@@ -38,6 +51,42 @@ public class MemoryTextPosition : MonoBehaviour
     void OnTriggerExit (Collider other) {
         if(other.transform == player) {
             m_IsPlayerInRange = false;
+            // FadeOutText();
         }
+    }
+
+    void FadeOutText(){
+        // fade from opaque to transparent
+        // loop over 1 second backwards
+        
+        Debug.Log("fade out");
+        for (float i = 5; i >= 0; i -= Time.deltaTime) {
+            // set color with i as alpha
+            Debug.Log(i);
+            memory.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, i);
+        }
+        m_IsTextVisible = false;
+    }
+
+    void FadeInText(){
+        // fade from transparent to opaque
+        // loop over 1 second
+        float waitTime = 10.0f;
+        float timer = 0.0f;
+
+        while (timer < waitTime)
+        {
+            timer += Time.deltaTime;
+            Debug.Log("current time: " + timer);
+
+            memory.GetComponent<TextMeshProUGUI>().alpha = timer;
+        }
+
+        // for (float i = 0; i <= 5; i += Time.deltaTime){
+        //     // set color with i as alpha
+        //     Debug.Log(i);
+        //     memory.GetComponent<TextMeshProUGUI>().color = new Color(1, 1, 1, i);
+        // }
+        m_IsTextVisible = true;
     }
 }
