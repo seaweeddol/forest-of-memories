716f9c334e62d956012c4840e33586ca9786650f
Jessica Stone <jstone4815@gmail.com>
2020-07-13 22:23:17 -0700

    initial commit

---
+++ Assets/Scripts/ShowTreeMemory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTreeMemory : MonoBehaviour
+{
+    // when player hovers mouse over tree, change icon to tree icon to indicate they can interact
+
+    // if they click, display the truncated memory in front of the player (will need to get player position, facing, etc)
+
+    // if they stay in range of the tree, scroll through the memory?
+    // or if they hover over the memory make the background more opaque and show the entire memory
+
+    // if the player leaves tree range, fade out the memory
+
+}
