1c275dd19c6e32a1386c8aa615ef059e491e0fc0
Jessica Stone <jstone4815@gmail.com>
2020-07-15 13:59:10 -0700

    clean up comments

--- Assets/Scripts/TreeInteraction.cs
+++ Assets/Scripts/TreeInteraction.cs
@@ -17,8 +17,6 @@ public class TreeInteraction : MonoBehaviour
     private Transform sentiment;
     private Transform memory;
 
-    // public Transform player;
-
     void Start(){
         treeInfo = tree.GetComponent<TreeInfo>();
         fullMemoryBook = fullMemoryContainer.transform.GetChild(0).gameObject;
@@ -31,19 +29,16 @@ public class TreeInteraction : MonoBehaviour
     // TODO: instead of mouseover, do a range? show icon or text when in range "Press E to view this memory"
 
     void OnMouseOver(){
-        // Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
         if (memoryText.GetComponent<TextMeshProUGUI>().alpha > 0.1){
             cursor.SetActive(true);
         }
     }
 
     void OnMouseExit(){
-        // Cursor.SetCursor(null, Vector2.zero, cursorMode);
         cursor.SetActive(false);
     }
 
     void OnMouseDown(){
-        // hide text on click or after a certain distance
         if(!fullMemoryBook.activeInHierarchy) {
             // TODO: player cannot move or look around when memory journal is open
             fullMemoryBook.SetActive(true);
