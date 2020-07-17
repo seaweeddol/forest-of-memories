a29cb96c9a1cdc1f8f130c62fe3e2168a5631922
Jessica Stone <jstone4815@gmail.com>
2020-07-17 13:04:13 -0700

    change text color

--- Assets/Scripts/MemoryTextPosition.cs
+++ Assets/Scripts/MemoryTextPosition.cs
@@ -42,7 +42,7 @@ public class MemoryTextPosition : MonoBehaviour
     void memoryPosition(){
         Vector3 playerDirection = player.forward;
         Quaternion playerRotation = player.rotation;
-        float memoryDistance = tree.transform.localScale.x * -1f;
+        float memoryDistance = tree.transform.localScale.x * -1.5f;
 
         Vector3 memoryPos = tree.transform.position + playerDirection * memoryDistance;
 
