7c102e10c138f688177c66d9252846891e9d1c1a
Jessica Stone <jstone4815@gmail.com>
2020-07-21 18:04:27 -0700

    add button hover effect

---
+++ Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        GameObject button = pointerEventData.pointerEnter;
+
+        button.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        GameObject button = pointerEventData.pointerEnter;
+
+        button.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
